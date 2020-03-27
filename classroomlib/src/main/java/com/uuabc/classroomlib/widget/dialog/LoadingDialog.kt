1f55cede7b5c5200263cf42317659fcd8c9a4b49
bin.wang <bin.wang@uuabc.com>
2020-03-27 12:18:15 +0800

    1.UU元素替换

--- classroomlib/src/main/java/com/uuabc/classroomlib/widget/dialog/LoadingDialog.kt
+++ classroomlib/src/main/java/com/uuabc/classroomlib/widget/dialog/LoadingDialog.kt
@@ -3,12 +3,9 @@ package com.uuabc.classroomlib.widget.dialog
 /**
  * Created by ln on 2019/1/25.
  */
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
 import android.app.Dialog
 import android.content.Context
 import android.view.ViewGroup
-import android.view.animation.LinearInterpolator
 import com.uuabc.classroomlib.R
 import kotlinx.android.synthetic.main.dialog_loading.*
 
@@ -16,12 +13,7 @@ fun createLoadingDialog(context: Context): Dialog {
     return Dialog(context, R.style.MyDialog_Loading).apply {
         setContentView(R.layout.dialog_loading)
         window?.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        val animator = ObjectAnimator.ofFloat(rotation, "rotation", -360f)
-        animator.duration = 1500
-        animator.interpolator = LinearInterpolator()
-        animator.repeatCount = ValueAnimator.INFINITE
-        animator.start()
+        lavLoading.imageAssetsFolder = "images/"
         setCancelable(false)
-        setOnDismissListener { animator.end() }
     }
 }
